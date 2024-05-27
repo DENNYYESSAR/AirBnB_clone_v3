6f58c348eb7a2e83a00e121b5ab9633f15ea2dc0
DENNYYESSAR <dennisokeri5@gmail.com>
2024-05-27 16:11:11 -0700

    user

--- models/user.py
+++ models/user.py
@@ -1,48 +1,66 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from hashlib import md5
-storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+""" User objects view for API v1 """
+from flask import jsonify, request, abort
+from api.v1.views import app_views
+from models import storage
+from models.user import User
 
 
-class User(BaseModel, Base):
-    """Representation of a user """
-    if storage_type == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column("password", String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    """ Retrieves the list of all User objects """
+    users = [user.to_dict() for user in storage.all(User).values()]
+    return jsonify(users)
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
 
-    @property
-    def password(self):
-        """
-        getter for password
-        :return: password (hashed)
-        """
-        return self.__dict__.get("password")
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """ Retrieves a User object """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
 
-    @password.setter
-    def password(self, password):
-        """
-        Password setter, with md5 hasing
-        :param password: password
-        :return: nothing
-        """
-        self.__dict__["password"] = md5(password.encode('utf-8')).hexdigest()
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_user(user_id):
+    """ Deletes a User object """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """ Creates a User """
+    if not request.json:
+        abort(400, 'Not a JSON')
+    data = request.get_json()
+    if 'email' not in data:
+        abort(400, 'Missing email')
+    if 'password' not in data:
+        abort(400, 'Missing password')
+    user = User(**data)
+    storage.new(user)
+    storage.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """ Updates a User object """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    if not request.json:
+        abort(400, 'Not a JSON')
+    data = request.get_json()
+    ignore_keys = ['id', 'email', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(user, key, value)
+    storage.save()
+    return jsonify(user.to_dict()), 200
